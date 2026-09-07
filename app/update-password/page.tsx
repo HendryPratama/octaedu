21ac920c35602cc93c87869e9e2a3ccd17ba730a
HendryPratama <pratamahendry45@gmail.com>
2026-09-07 20:49:07 +0700

    edit update password

--- app/update-password/page.tsx
+++ app/update-password/page.tsx
@@ -1,7 +1,5 @@
 'use client';
 
-export const dynamic = 'force-dynamic'; 
-export const revalidate = 0;
 
 import { useActionState } from 'react';
 import { updateNewPassword } from '@/app/actions/auth';
